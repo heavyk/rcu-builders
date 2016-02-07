3bc35c984119ff453ff33864c203d547d7c3e3f3
Rich-Harris <richard.a.harris@gmail.com>
2016-02-06 22:30:54 -0500

    add es6 tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 import * as sander from 'sander';
 import * as path from 'path';
 import nodeResolve from 'resolve';
+import { transform } from 'babel-core';
 import Ractive from 'ractive';
 import { init, parse, resolve } from 'rcu';
 import { describe, it } from 'mocha';
@@ -103,6 +104,59 @@ describe( 'rcu-builders', () => {
 							fn( module, module.exports, req );
 
 							return module.exports;
+						});
+				});
+		}
+
+		samples.forEach( sample => {
+			it( sample.title, () => {
+				return load( sample.entry ).then( sample.test );
+			});
+		});
+	});
+
+	describe( 'es', () => {
+		let components = {};
+
+		function load ( file ) {
+			return sander.readFile( file, { encoding: 'utf-8' })
+				.then( source => parse( source ) )
+				.then( definition => {
+					const importPromises = definition.imports.map( ({ href }) => {
+						const resolved = resolve( href, file );
+
+						return load( resolved ).then( Component => {
+							components[ resolved ] = Component;
+						});
+					});
+
+					return Promise.all( importPromises )
+						.then( () => {
+							let { code } = es6( definition );
+
+							code = transform( code, {
+								presets: [ 'es2015' ],
+								babelrc: false
+							}).code;
+
+							const fn = new Function( 'module', 'exports', 'require', code );
+
+							function req ( relativePath ) {
+								let resolved = resolve( relativePath, file ) + '.html';
+
+								if ( components[ resolved ] ) return components[ resolved ];
+
+								resolved = nodeResolve.sync( relativePath, {
+									base: path.dirname( file )
+								});
+
+								return require( resolved );
+							}
+
+							let module = { exports: {} };
+							fn( module, module.exports, req );
+
+							return module.exports.default;
 						});
 				});
 		}
