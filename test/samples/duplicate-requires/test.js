b8fd67d934629648f1c9aa312ad4358d47bb85a3
flames of love <mechanicofthesequence@gmail.com>
2016-03-09 18:22:31 +0100

    improve tests

--- test/samples/duplicate-requires/test.js
+++ test/samples/duplicate-requires/test.js
@@ -5,6 +5,8 @@ export default function test ( Component ) {
 
 	assert.equal( ractive.get( 'foo' ), 'FOO' );
 	assert.equal( ractive.get( 'bar' ), 'BAR' );
+	assert.equal( ractive.get( 'baz' ), 'BAZ' );
 	assert.equal( ractive.get( 'x' ), require( __dirname + '/x' ) );
 	assert.equal( ractive.decorators.deco, require( __dirname + '/x' ) );
+	assert.equal( ractive.decorators.ration, require( __dirname + '/y' ) );
 }
